cff6bb5ad85aef8aa423716714ba821633c9de7a
wizari <git301398>
2019-05-21 20:47:57 +0300

    Initial

---
+++ android/src/com/gmail/wizaripost/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gmail.wizaripost;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gmail.wizaripost.Sapper;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Sapper(), config);
+	}
+}
---
+++ desktop/src/com/gmail/wizaripost/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.gmail.wizaripost.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gmail.wizaripost.Sapper;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sapper(), config);
+	}
+}
+K
